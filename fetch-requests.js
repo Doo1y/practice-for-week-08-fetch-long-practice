a820a4fe07a3c8fdc6291fe8dbfc7bc8d2c3a3e9
Samuel Park <s.park111497@gmail.com>
2022-10-03 14:08:29 -0700

    finished assignment

--- fetch-requests.js
+++ fetch-requests.js
@@ -7,14 +7,74 @@ Paste your code for fetch requests here once you finish each phase.
 
 // Your code here
 
+function phaseOne() {
+  const reqOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+  }
 
+  fetch("/products", reqOptions)
+}
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
+function phaseTwo() {
+  const reqOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+  }
 
+  fetch("/products", reqOptions)
+    .then(response => {
+      console.log(response.status);
+      console.log(response.headers.get("Content-Type"));
+      console.log(response.url);
+      return response.text();
+    })
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
 
 
 /* ============================== Phase 3 ============================== */
 
 // Your code here
+function parse(reqBody) {
+  return reqBody
+    .split("&")
+    .map(keyValuePair => keyValuePair.split("="))
+    .map(([key, value]) => [
+      key,
+      decodeURIComponent(value)
+    ])
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+}
+function phaseThree() {
+  const reqOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams(parse("name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"))
+  }
+
+  fetch("/products", reqOptions)
+    .then(response => {
+      console.log(response.status);
+      console.log(response.headers.get("Content-Type"));
+      console.log(response.url);
+      return response.text();
+    })
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }
